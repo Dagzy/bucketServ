31baf3fa0367e8d23771483c8255c3610b6ef93c
Dagzy <quincye.williams@gmail.com>
2017-07-19 17:36:38 -0400

    QW prepared for deployment

--- router.js
+++ router.js
@@ -8,12 +8,12 @@ const requireAuth = passport.authenticate('jwt', {session:false});
 const requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function(app){
-	app.post('/signup', Auth.signup);
-	app.post('/signin', requireSignin, Auth.signin);
-	app.post('/new-item', requireAuth, BucketList.addBucketList);
-	app.get('/items', requireAuth, BucketList.fetchBucketLists);
-	app.delete('/items/:id', requireAuth, BucketList.deleteBucketList);
-	app.get('/items/:id', requireAuth, BucketList.fetchBucketList);
-	app.put('/items/:id', requireAuth, BucketList.updateBucketList);
+	app.post('/api/signup', Auth.signup);
+	app.post('/api/signin', requireSignin, Auth.signin);
+	app.post('/api/new-item', requireAuth, BucketList.addBucketList);
+	app.get('/api/items', requireAuth, BucketList.fetchBucketLists);
+	app.delete('/api/items/:id', requireAuth, BucketList.deleteBucketList);
+	app.get('/api/items/:id', requireAuth, BucketList.fetchBucketList);
+	app.put('/api/items/:id', requireAuth, BucketList.updateBucketList);
 }
 //console.log(req.body);
